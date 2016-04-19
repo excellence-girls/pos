7d817ef46939a4809fdcf1837746952c182368ca
wangdan <1253864861@qq.com>
2016-04-19 18:28:31 +0800

    3

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,19 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
 
+ var item = {items:inputs,allPrice:0} ;
+  var subtTotal ;
+  var accounts = '';
+    for(var i = 0 ; i < inputs.length; i++)
+    {
+      item.items[i].subtTotal = inputs[i].price * inputs[i].count;
+      item.allPrice+= item.items[i].subtTotal;
+      
+      accounts+= '名称：' + inputs[i].name +',' + '数量：' + inputs[i].count
+      +inputs[i].unit +',' + '单价：' + inputs[i].price +'.00(元)，小计：' +
+       item.items[i].subtTotal + '.00(元)' + '\n'; 
+    } 
+    accounts = '***<没钱赚商店>收据***' + '\n' + accounts + '----------------------' + '\n' +'总计：' +
+     item.allPrice + '.00(元)' +'\n' +'**********************';
+     console.log(accounts) ;
 }
