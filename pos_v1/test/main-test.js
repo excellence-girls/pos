054b4c1ae8b8fa0cd53d29f555f4c2bff082ea59
wangdan <1253864861@qq.com>
2016-04-21 20:37:30 +0800

    changed jasmine

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -16,24 +16,76 @@ describe('pos', function() {
       'ITEM000005'
     ];
   });
+ describe('unit test',function() {
+   describe('buildCartItems test', function () {
 
-  it('should print correct text', function() {
+     it('buildCartItems test output:', function () {
 
-    spyOn(console, 'log');
+       spyOn(console, 'log');
 
-    printReceipt(inputs);
+       printReceipt(inputs);
 
-    var expectText =
-      '***<没钱赚商店>收据***\n' +
-      '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
-      '----------------------\n' +
-      '总计：51.00(元)\n' +
-      '节省：7.50(元)\n' +
-      '**********************';
+       var expectText =[
+         {item: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3}, count: 5},
+         {item: {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15}, count: 2},
+         {item: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5}, count: 3}];
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
-  });
+       expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+   });
+   describe('receiptItems test', function () {
+
+     it('receiptItems test output:', function () {
+
+       spyOn(console, 'log');
+
+       printReceipt(inputs);
+
+       var expectText =[
+         {cartItem:{item: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3}, count: 5}, savingSubTotal: 3, subTotal: 12},
+         {cartItem:{item: {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15}, count: 2}, savingSubTotal: 0, subTotal: 30},
+         {cartItem:{item: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5}, count: 3}, savingSubTotal: 4.5, subTotal: 9}];
+       expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+   });
+   describe('receiptItems test', function () {
+
+     it('receiptItems test output:', function () {
+
+       spyOn(console, 'log');
+
+       printReceipt(inputs);
+
+       var expectText ={receiptItems:[
+       {cartItem:{item: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3}, count: 5}, savingSubTotal: 3, subTotal: 12},
+       {cartItem:{item: {barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15}, count: 2}, savingSubTotal: 0, subTotal: 30},
+       {cartItem:{item: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5}, count: 3}, savingSubTotal: 4.5, subTotal: 9}],
+       savingTotal :7.5,total:51}
+     ;
+       expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+   });
+   describe('receiptText test', function () {
+
+     it('receiptText test output:', function () {
+
+       spyOn(console, 'log');
+
+       printReceipt(inputs);
+
+       var expectText =
+        '***<没钱赚商店>收据***\n' +
+        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+        '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+        '----------------------\n' +
+        '总计：51.00(元)\n' +
+        '节省：7.50(元)\n' +
+        '**********************';
+
+       expect(console.log).toHaveBeenCalledWith(expectText);
+     });
+   });
+ });
 });
 
